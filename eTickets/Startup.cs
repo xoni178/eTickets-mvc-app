99e791c290c1db4d1443cd7606bb57f8e1b5d62d
Egzon Krasniqi <egzoni805@gmail.com>
2024-06-13 12:55:23 +0200

    fix

--- eTickets/Startup.cs
+++ eTickets/Startup.cs
@@ -28,7 +28,6 @@ namespace eTickets
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
 
@@ -64,7 +63,7 @@ namespace eTickets
             services.AddControllersWithViews();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -74,7 +73,7 @@ namespace eTickets
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+             
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
